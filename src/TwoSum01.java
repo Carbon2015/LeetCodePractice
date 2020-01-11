015c03f03d13654e6c801b5dfedcf88c09cd4dc2
Carbon <opensail@163.com>
2020-01-11 23:28:44 +0800

    two-sums-01

--- src/TwoSum01.java
+++ src/TwoSum01.java
@@ -1,9 +1,37 @@
+import java.util.Arrays;
+
 public class TwoSum01 {
     public static void main(String[] args) {
-
+        int[] testData = {3, 2, 4};
+        int target = 6;
+        int[] result = {0, 0};
+        result = twoSum(testData, target);
     }
 
     public static int[] twoSum(int[] nums, int target) {
-
+        int numsLength = nums.length;
+        for (int i = 0; i < numsLength; i++) {
+            for (int j = i + 1; j < numsLength; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("no such two items in array");
     }
+
+//    public static int[] twoSum1(int[] nums, int target) {
+//        Arrays.sort(nums);
+//        for (int prevPointer = 0, rearPointer = nums.length - 1; prevPointer < rearPointer; ) {
+//            int tempSum = nums[prevPointer] + nums[rearPointer];
+//            if (tempSum == target) {
+//                return new int[]{prevPointer, rearPointer};
+//            } else if (tempSum > target) {
+//                rearPointer--;
+//            } else {
+//                prevPointer++;
+//            }
+//        }
+//        throw new IllegalArgumentException("no such two items in array");
+//    }
 }
