bcdcb3e60457b028fa82808487362c1f9644d21c
Carbon <opensail@163.com>
2020-01-12 09:58:11 +0800

    optimize MoveZeroes283

--- src/MoveZeroes283.java
+++ src/MoveZeroes283.java
@@ -6,12 +6,13 @@ public class MoveZeroes283 {
 
     public static void moveZeroes(int[] nums) {
         int firstZero = 0;
-        for (int i = 0; i < nums.length; i++) {
+        int numsLength = nums.length;
+        for (int i = 0; i < numsLength; i++) {
             if (nums[i] != 0) {
                 nums[firstZero++] = nums[i];
             }
         }
-        for (int i = firstZero; i < nums.length; i++) {
+        for (int i = firstZero; i < numsLength; i++) {
             nums[i] = 0;
         }
     }
